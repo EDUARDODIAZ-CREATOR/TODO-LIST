bdd7b05564598d6a5a80d82f3b490b727b497cea
Julio Eduardo Angeles Diaz <julio.angeles.diaz.0609@gmail.com>
2025-10-26 21:15:58 -0600

    fix: terminacion de los metodos o funciones comunes

--- todo-app/src/store/todo.strore.js
+++ todo-app/src/store/todo.strore.js
@@ -64,7 +64,7 @@ const setFilter = ( newFilter = Filters.All ) => {
 }
 
 const getCurrentFilter = () => {
-    throw new Error('Function not implemented.');
+    return state.filter;
 }
 
 const getTodos = ( filter = Filters.All ) => {
